9f5594118692e47950406c5eb642a1f9b997ff23
rohit528590 <rohit528590@icloud.com>
2025-11-12 14:20:43 +0530

    Pair Sum Problem Brute Force Apporach

---
+++ Moore's_Voting_Algorithm/01_PairSum-BruteApporach.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> PairSum(vector<int> nums, int target)
+{
+    int n = nums.size();
+    vector<int> ans;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
+        }
+    }
+    
+    return ans;
+}
+
+int main()
+{
+    cout << "Welcome to the Pair Sum Problem!\n";
+    vector<int> nums = {2, 7, 11, 15};
+
+    cout << "Given Vector : ";
+    for (int i : nums)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    int target = 13;
+    vector<int> ans = PairSum(nums, target);
+    cout << "nums[" << ans[0] << "] + nums[" << ans[1] << "] = " << target << endl;
+
+    return 0;
+}
