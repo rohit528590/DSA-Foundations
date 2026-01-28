896d4c87d3ff3a3ce84c2c3163beaccd071bbbc5
rohit528590 <rohit528590@icloud.com>
2026-01-28 21:13:54 +0530

    Binary Search Algorithm (Optimal)

---
+++ Binary_Search_Algorithm/binarySearchAlgo_Optimal.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int> nums, int target)
+{
+    int start = 0, end = nums.size() - 1;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2; // Prevents overflow when start + end exceeds int range 
+        if (nums[mid] > target)
+        {
+            end = mid - 1;
+        }
+        else if (nums[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    cout << "Welcome to Binary Search Algorithm!\n";
+
+    vector<int> nums = {-1, 0, 3, 4, 5, 9, 12};
+    int target;
+
+    cout << "Enter which number you want to search : ";
+    cin >> target;
+
+    int result = binarySearch(nums, target);
+    (result != -1) ? cout << target << " found at index " << result << ".\n" : cout << "Number not found.\n";
+
+    return 0;
+}
